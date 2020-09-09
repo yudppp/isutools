c753907b6a5c9a70110103ff4ab449c6326ec819
yudppp <yu.d.ppp@gmail.com>
2020-09-10 01:26:10 +0900

    add: Add slack cat  token

--- utils/slackcat/slackcat.go
+++ utils/slackcat/slackcat.go
@@ -2,45 +2,52 @@ package slackcat
 
 import (
 	"fmt"
+	"io"
 	"path"
 	"strings"
 	"time"
-	"io"
+
 	// "bytes"
 	"os/exec"
 )
 
 var defaultChannel = "notification"
+var tokenOption = ""
 
 // SendFile .
 func SendFile(filePath, pattern string) error {
 	ext := path.Ext(pattern)
 	names := strings.Split(pattern, ".")
 	outputName := fmt.Sprintf("%s-%s%s", names[0], time.Now().Format("200601021504"), ext)
-	return exec.Command("slackcat", "--channel", defaultChannel, "-n", outputName, filePath).Run()
+	fmt.Println("slackcat", "--channel", defaultChannel, tokenOption, "-n", outputName, filePath)
+	return exec.Command("slackcat", "--channel", defaultChannel, tokenOption, "-n", outputName, filePath).Run()
 }
 
 // SendText .
 func SendText(filename, text string) error {
 	c1 := exec.Command("echo", text)
-    c2 := exec.Command("slackcat", "--channel", defaultChannel, "--filename", filename)
+	c2 := exec.Command("slackcat", "--channel", defaultChannel, tokenOption, "--filename", filename)
 
-    r, w := io.Pipe()
-    c1.Stdout = w
+	r, w := io.Pipe()
+	c1.Stdout = w
 	c2.Stdin = r
-	
-	// var out bytes.Buffer
-    // c2.Stdout = &out
 
-    c1.Start()
-    c2.Start()
-    c1.Wait()
-    w.Close()
-    return  c2.Wait()		
-	// return nil
+	c1.Start()
+	c2.Start()
+	c1.Wait()
+	w.Close()
+	return c2.Wait()
 }
 
 // SetDefaultChannel .
 func SetDefaultChannel(channel string) {
 	defaultChannel = channel
+}
+
+// SetToken .
+func SetToken(token string) {
+	if token == "" {
+		tokenOption = ""
+	}
+	tokenOption = fmt.Sprintf("--token %s", token)
 }
